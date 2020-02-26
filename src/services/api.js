b5071a9c61a876fdc0bf681f2c61d25477fc7cb7
Fábio Souza <fabiobarboza7@hotmail.com>
2020-02-26 01:03:42 -0300

    http missing s

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://fake-cashback-api.herokuapp.com/api/v1',
+  baseURL: 'https://fake-cashback-api.herokuapp.com/api/v1',
 });
 
 export default api;
